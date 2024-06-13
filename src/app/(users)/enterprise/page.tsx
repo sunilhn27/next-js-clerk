ce4a2e30564aef8bba8c25e60543a0c2b59dcf3b
Sunil H N <sunilh.n27@gmail.com>
2024-06-14 00:39:43 +0530

    Changes in EnterPrise user Page

--- src/app/(users)/enterprise/page.tsx
+++ src/app/(users)/enterprise/page.tsx
@@ -1,7 +1,21 @@
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 function EnterPriseuserPage() {
-  return <div>EnterPriseuserPage</div>;
+  const { sessionClaims } = auth();
+
+  if (sessionClaims?.metadata.role !== "enterprise")
+    return (
+      <div className="flex justify-center items-center">
+        You are not Enterprise User
+      </div>
+    );
+
+  return (
+    <div className="flex justify-center items-center">
+      Welcome EnterPrise user Page
+    </div>
+  );
 }
 
 export default EnterPriseuserPage;
