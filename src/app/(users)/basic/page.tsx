01a3bdbdce003a2ade23000a2e5176e07b657e21
Sunil H N <sunilh.n27@gmail.com>
2024-06-14 00:39:32 +0530

    Changes in Basic user Page

--- src/app/(users)/basic/page.tsx
+++ src/app/(users)/basic/page.tsx
@@ -1,7 +1,21 @@
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 function BasicuserPage() {
-  return <div>BasicuserPage</div>;
+  const { sessionClaims } = auth();
+
+  if (sessionClaims?.metadata.role !== "basic")
+    return (
+      <div className="flex justify-center items-center">
+        You are not Basic User
+      </div>
+    );
+
+  return (
+    <div className="flex justify-center items-center">
+      Welcome Basic user Page
+    </div>
+  );
 }
 
 export default BasicuserPage;
