6ceeb1c05c06df588d3d27b25405c07874a5f2de
Sunil H N <sunilh.n27@gmail.com>
2024-06-14 00:39:52 +0530

    Changes in Premium user Page

--- src/app/(users)/premium/page.tsx
+++ src/app/(users)/premium/page.tsx
@@ -1,7 +1,21 @@
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 
 function PremiumuserPage() {
-  return <div>PremiumuserPage</div>;
+  const { sessionClaims } = auth();
+
+  if (sessionClaims?.metadata.role !== "premium")
+    return (
+      <div className="flex justify-center items-center">
+        You are not Premium User
+      </div>
+    );
+
+  return (
+    <div className="flex justify-center items-center">
+      Welcome PremiumuserPage
+    </div>
+  );
 }
 
 export default PremiumuserPage;
