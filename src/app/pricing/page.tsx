65fa9ddc7885bd0681ca5e520bd6ffb5872e13a1
Sunil H N <sunilh.n27@gmail.com>
2024-06-17 22:58:49 +0530

    added Pricing page

---
+++ src/app/pricing/page.tsx
@@ -0,0 +1,12 @@
+import Pricing from "@/components/Pricings";
+import React from "react";
+
+function PricingPage() {
+  return (
+    <>
+      <Pricing />
+    </>
+  );
+}
+
+export default PricingPage;
